0f9dec8f0b344fdb9386298cea32b8136e8990a6
Samuobe <samuobe@ik.me>
2025-07-22 22:10:52 +0200

    Refactor intent matching logic to improve accuracy and readability

--- pyntent.py
+++ pyntent.py
@@ -1,43 +1,45 @@
+import string
+
 def get_intent(phrase):
     result_id = 0
     result_cent = 0
     with open("intent/INDEX.txt", "r") as f:
         data_file = f.readlines()
     
-    #print(data_file)
-    
     for data in data_file:
         intent_data = data.split("|")
         intent_id = intent_data[0]
-        intent_file = str(intent_data[1])
-        intent_file = intent_file.replace("\n", "")
+        intent_file = str(intent_data[1]).strip()
         
         with open("./intent/"+intent_file+".txt") as file:
             intent_verify_data = file.readlines()
+        
+        total_percentage = 0
+        line_count = 0
 
-        words_tot_verify = 0
-        words_verify_present = 0
         for line in intent_verify_data:
             words_count_verify = line.split(" ")
+            words_tot_verify = len(words_count_verify)
+            words_verify_present = 0
+            
+            phrase_words = phrase.lower().translate(str.maketrans('', '', string.punctuation)).split()
             for word in words_count_verify:
-                if word.lower() in phrase.lower():
-                    words_verify_present = words_verify_present + 1
-                words_tot_verify = words_tot_verify + 1
+                word = word.strip().lower().translate(str.maketrans('', '', string.punctuation))
+                if word in phrase_words:
+                    words_verify_present += 1
+            
+            if words_tot_verify > 0:
+                line_percentage = (words_verify_present / words_tot_verify) * 100
+                total_percentage += line_percentage
+                line_count += 1
 
-        result_temp = (words_verify_present / words_tot_verify) * 100
+        if line_count > 0:
+            average_percentage = total_percentage / line_count
+            if average_percentage > result_cent:
+                result_id = intent_id
+                result_cent = average_percentage
 
-        if result_temp > result_cent:
-            result_id = intent_id
-            result_cent = result_temp
-
-    #print("RESULT ID="+str(result_id))
-    #print("RESULT CENT="+str(result_cent))
-
-    return(int(intent_id), float(result_cent))
-
-
-        
-
+    return int(result_id), float(result_cent)
 
 
-#get_intent("Che tempo c'è oggi?")
+#print(get_intent("Che tenmpo fa otra?"))
